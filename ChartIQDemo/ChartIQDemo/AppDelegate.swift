8e32c466bd403d80b32ba58f8deeb5d068b45305
Ryan McNeely <RMcNeely@users.noreply.github.com>
2018-11-15 16:53:48 -0500

    Update Pod and remove Roko methods relating to users (#5)
    * Update Pod and remove Roko methods relating to users
    
    * Use Cocoapod 2.0.3

--- ChartIQDemo/ChartIQDemo/AppDelegate.swift
+++ ChartIQDemo/ChartIQDemo/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         //try! ChartIQView.start(withAPIKey: apiKey, url: url)
-        try! ChartIQView.start(withAPIKey: apiKey, url: (url))
+        try! ChartIQView.start(url: (url))
         return true
     }
 
